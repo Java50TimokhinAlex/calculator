410c4affea02bd2334307bb28912b68a61728c8f
Alex <alexthuder87@gmail.com>
2023-03-22 21:01:11 +0200

    initial code Calculator and test

---
+++ src/telran/numbers/Calculator.java
@@ -0,0 +1,20 @@
+package telran.numbers;
+
+public class Calculator {
+	static public int multiply(int op1, int op2) {
+		return op1 * op2;
+	}
+	static public int sum(int op1, int op2) {
+		return op1 + op2;
+	}
+	static public int subtract(int op1, int op2) {
+		return op1 - op2;
+	}
+	static public int delay(int op1, int op2) {
+		return op1 / op2;
+	}
+	static public int remainder(int op1, int op2) {
+		return op1 % op2;
+	}
+}
+	
